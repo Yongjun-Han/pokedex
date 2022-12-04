0e44dbcd0e5da8ddde2ab3144e0e42c6df5a0534
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-04 23:50:16 +0900

    Initial Commit

---
+++ PokemonDex/PokemonDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDexApp.swift
+//  PokemonDex
+//
+//  Created by YongJun Han on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
